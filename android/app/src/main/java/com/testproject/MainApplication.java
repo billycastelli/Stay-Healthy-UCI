0d3abbc911e86f078cd2b8ce51c6cbad7bf11574
William Castelli <billycastelli@gmail.com>
2020-02-18 12:18:31 -0800

    Dependencies

--- android/app/src/main/java/com/testproject/MainApplication.java
+++ android/app/src/main/java/com/testproject/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
