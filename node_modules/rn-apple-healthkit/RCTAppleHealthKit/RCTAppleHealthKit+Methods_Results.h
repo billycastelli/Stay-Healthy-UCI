b93cb12daafe1ed216656bed2e58693870ad0740
Jas Delgado <jas@jasdelgado.com>
2020-03-05 10:58:53 -0800

    remove node_modules before merging branches

--- node_modules/rn-apple-healthkit/RCTAppleHealthKit/RCTAppleHealthKit+Methods_Results.h
+++
@@ -1,7 +0,0 @@
-#import "RCTAppleHealthKit.h"
-
-@interface RCTAppleHealthKit (Methods_Results)
-
-- (void)results_getBloodGlucoseSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
-
-@end
